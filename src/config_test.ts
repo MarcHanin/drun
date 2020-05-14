cc923a7680df1cf10ae7990a9e36b482fed149f1
Marc Hanin <marchanin@hotmail.fr>
2020-05-14 18:09:16 +0200

    Add scope in tests

--- src/config_test.ts
+++ src/config_test.ts
@@ -2,7 +2,7 @@ import { assertEquals, assertThrowsAsync } from '../dev_deps.ts';
 import { loadConfig } from './config.ts';
 import { DrunConfigValidationError } from './errors/drun_config_validation_error.ts';
 
-Deno.test('valid config with entry point', async () => {
+Deno.test('[config] valid config with entry point', async () => {
   const config = await loadConfig({ entryPoint: 'testFile.ts' });
   const expected = {
     cwd: './',
@@ -13,14 +13,14 @@ Deno.test('valid config with entry point', async () => {
   assertEquals(config, expected);
 });
 
-Deno.test('valid config with excludes', async () => {
+Deno.test('[config] valid config with excludes', async () => {
   const config = await loadConfig({ entryPoint: 'testFile.ts', excludes: ['src/testData/file_to_exclude.ts'] });
   const expected = 1;
 
   assertEquals(config.excludes.length, expected);
 });
 
-Deno.test('valid config with CWD', async () => {
+Deno.test('[config] valid config with CWD', async () => {
   const config = await loadConfig({ entryPoint: 'testFile.ts', cwd: './testData' });
   const expected = {
     cwd: './testData',
@@ -31,25 +31,25 @@ Deno.test('valid config with CWD', async () => {
   assertEquals(config, expected);
 });
 
-Deno.test('invalid cwd', async () => {
+Deno.test('[config] invalid cwd', async () => {
   assertThrowsAsync(async () => {
     await loadConfig({ entryPoint: 'testFile.ts', cwd: 1 });
   }, DrunConfigValidationError, 'CWD must be a string');
 });
 
-Deno.test('missing entryPoint', async () => {
+Deno.test('[config] missing entryPoint', async () => {
   assertThrowsAsync(async () => {
     await loadConfig({})
   }, DrunConfigValidationError, 'Entry point is required');
 });
 
-Deno.test('invalid entryPoint', async () => {
+Deno.test('[config] invalid entryPoint', async () => {
   assertThrowsAsync(async () => {
     await loadConfig({ entryPoint: 1 });
   }, DrunConfigValidationError, 'Entry point must be a string');
 });
 
-Deno.test('invalid entryPoint extension', async () => {
+Deno.test('[config] invalid entryPoint extension', async () => {
   assertThrowsAsync(async () => {
     await loadConfig({ entryPoint: 'badEntryPoint.rs' });
   }, DrunConfigValidationError, 'Entry point must be one of the following types: .js, .ts');
--- src/parse_arguments_test.ts
+++ src/parse_arguments_test.ts
@@ -1,35 +1,35 @@
 import { assertEquals } from '../dev_deps.ts';
-import { parseArguments } from './arguments.ts';
+import { parseArguments } from './parse_arguments.ts';
 
-Deno.test('parse with empty arguments', () => {
+Deno.test('[parseArguments] parse with empty arguments', () => {
   const args = [] as string[];
   const expected = {};
 
   assertEquals(parseArguments(args), expected);
 });
 
-Deno.test('parse with unknown argument', () => {
+Deno.test('[parseArguments] parse with unknown argument', () => {
   const args = ['--unknownArg=true'];
   const expected = {};
 
   assertEquals(parseArguments(args), expected);
 });
 
-Deno.test('parse with entryPoint argument', () => {
+Deno.test('[parseArguments] parse with entryPoint argument', () => {
   const args = ['--entryPoint=main.ts'];
   const expected = { entryPoint: 'main.ts' };
 
   assertEquals(parseArguments(args), expected);
 });
 
-Deno.test('parse with cwd argument', () => {
+Deno.test('[parseArguments] parse with cwd argument', () => {
   const args = ['--cwd=cwdFolder'];
   const expected = { cwd: 'cwdFolder' };
 
   assertEquals(parseArguments(args), expected);
 });
 
-Deno.test('parse with all arguments', () => {
+Deno.test('[parseArguments] parse with all arguments', () => {
   const args = [
     '--entryPoint=main.ts',
     '--cwd=cwdFolder',
