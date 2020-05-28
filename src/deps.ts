5b766a1bf0f5be0344dd3e49a4bc653b19b45147
mthsmb <matheusmb1919@gmail.com>
2020-05-28 03:21:35 -0300

    Revert commit

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { readJson } from "https://deno.land/std@v0.53.0/fs/mod.ts";
-export { extname } from "https://deno.land/std@v0.53.0/path/mod.ts";
+export { readJson } from "https://deno.land/std@v0.52.0/fs/mod.ts";
+export { extname } from "https://deno.land/std@v0.52.0/path/mod.ts";
