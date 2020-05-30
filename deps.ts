a64b9a3527e521bf1adc43c5e509138ccacc0d2f
Marc Hanin <marchanin@hotmail.fr>
2020-05-31 01:14:05 +0200

    Upgrade to deno std v0.54 (#12)

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { readJson } from "https://deno.land/std@v0.52.0/fs/mod.ts";
-export { extname } from "https://deno.land/std@v0.52.0/path/mod.ts";
-export * as log from "https://deno.land/std@v0.53.0/log/mod.ts";
+export { readJson } from "https://deno.land/std@v0.54.0/fs/mod.ts";
+export { extname } from "https://deno.land/std@v0.54.0/path/mod.ts";
+export * as log from "https://deno.land/std@v0.54.0/log/mod.ts";
