7acedb8a94de1b2e9a18a825c84c74de00c7b4df
Marc Hanin <marchanin@hotmail.fr>
2020-07-26 22:46:08 +0200

    Upgrade to Deno std v0.62.0 (#22)

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@v0.54.0/testing/asserts.ts";
-export { existsSync } from "https://deno.land/std@v0.54.0/fs/mod.ts";
+} from "https://deno.land/std@v0.62.0/testing/asserts.ts";
+export { existsSync } from "https://deno.land/std@v0.62.0/fs/mod.ts";
